bb883af0b634db04e8653c149e9123c96ed1d1b4
Sedana Yoga <9g.yoga27@gmail.com>
2024-07-18 12:03:31 +0800

    fix: noice issue

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -36,4 +36,20 @@ return {
       { "<leader>/", false },
     },
   },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    -- REMOVE THIS once this issue is fixed: https://github.com/yioneko/vtsls/issues/159
+    opts = {
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "Request textDocument/inlayHint failed",
+          },
+          opts = { skip = true },
+        },
+      },
+    },
+  },
 }
