02c995fcca6cb9919cd176eab36aa4cd15e107ba
Sedana Yoga <9g.yoga27@gmail.com>
2024-11-20 09:17:37 +0800

    fix: update lazyvim and snack.nvim

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,4 +2,15 @@ return {
   -- disable trouble
   { "rcarriga/nvim-notify", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      notifier = {
+        top_down = false,
+      },
+      dashboard = {
+        enabled = false,
+      },
+    },
+  },
 }
