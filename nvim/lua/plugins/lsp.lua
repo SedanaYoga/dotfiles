8da490f55cf1ac6b429525481aa37be0a95f5a7f
Sedana Yoga <9g.yoga27@gmail.com>
2025-07-16 21:22:14 +0800

    fix: tailwind

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -16,6 +16,23 @@ return {
             },
           },
         },
+        tailwindcss = {
+          root_dir = function(fname)
+            local node_modules = vim.fs.find("node_modules", { path = fname, upward = true })[1]
+            local root = vim.fs.dirname(node_modules)
+            if not node_modules then
+              return nil
+            else
+              local tailwind_path = node_modules .. "/tailwindcss"
+
+              if vim.fn.isdirectory(tailwind_path) ~= 0 then
+                return root
+              else
+                return nil
+              end
+            end
+          end,
+        },
       },
 
       setup = {
